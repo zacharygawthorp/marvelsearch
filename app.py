6876504927a3ddbaaf04e434f81882be8ee1174d
zacharygawthorp <107508857+zacharygawthorp@users.noreply.github.com>
2022-10-11 11:45:46 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -25,10 +25,10 @@ BASE_URL = 'https://gateway.marvel.com/v1/public'
 app = Flask(__name__)
 
 """Resolve postgres / postgresql url path error with Heroku."""
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
 uri = os.environ.get('DATABASE_URL')
 if uri.startswith("postgres://"):
   uri = uri.replace("postgres://", "postgresql://", 1)
+  app.config['SQLALCHEMY_DATABASE_URI'] = uri
   
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
